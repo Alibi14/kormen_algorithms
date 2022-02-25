23d2b0167d0fc7e064ddba10ccf046a1f8c7b463
Alibi14 <wap.ali.kz@gmail.com>
2022-02-25 17:13:40 +0600

    next problem solved

--- 2.1-2.py
+++ 2.1-2.py
@@ -1,17 +1,11 @@
 
-
 def insertion_sort(nums: list):
 
     for i in range(1, len(nums)):
         key = nums[i]
         j = i - 1
-        while j >= 0 and nums[j] > key:
+        while j >= 0 and nums[j] < key:
             nums[j+1] = nums[j]
             j = j - 1
 
         nums[j+1] = key
-
-    print(nums)
-
-
-insertion_sort([1, 5, 4, 8, 3, 9])
